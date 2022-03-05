8f4a7aabe39079715fd20ee198e3474e90d0ab06
Ruta Bendoraityte <90304014+rutaben@users.noreply.github.com>
2022-03-05 21:09:22 +0200

    bugFix: edited navbar font

--- client/src/components/partials/navbar/styled-nav-link.jsx
+++ client/src/components/partials/navbar/styled-nav-link.jsx
@@ -2,11 +2,15 @@ import { styled } from '@mui/material/styles';
 import { NavLink } from 'react-router-dom';
 
 const StyledNavlink = styled(NavLink)(({ theme }) => ({
+  fontSize: '1.05rem',
   fontWeight: 300,
-  textDecoration: 'none',
+  textDecoration: 'underline',
+  textUnderlineOffset: '0.2rem',
+  textDecorationColor: 'rgba(0,0,0,0.0)',
+  backgroundColor: 'rgba(0,0,0,0.0)',
   color: theme.palette.primary.contrastText,
   '&.active': {
-    fontWeight: 400,
+    textDecorationColor: theme.palette.primary.contrastText,
   },
 }));
 
